3d55d027b1f60b14e183129d40afadaf41ec13a3
Sneider Castillo <sneicast@hotmail.com>
2021-07-04 12:10:26 -0500

    iniciar proyecto componentes

---
+++ componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  const HomePageTemp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          'Hola',
+        ),
+      ),
+    );
+  }
+}
