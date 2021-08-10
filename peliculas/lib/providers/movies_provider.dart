7b20bcbe407e35c15199db75fae53ba0f300ba8a
Sneider Castillo <sneicast@hotmail.com>
2021-08-09 21:24:49 -0500

    app peliculas finalizada

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/helpers/debouncer.dart';
 import 'package:peliculas/models/models.dart';
 import 'package:peliculas/models/popular_response.dart';
+import 'package:peliculas/models/search_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = 'f58129231efff828be8fc55df2052380';
@@ -15,13 +19,22 @@ class MoviesProvider extends ChangeNotifier {
 
   int _popularPage = 0;
 
+  final debouncer = Debouncer(
+    duration: Duration(milliseconds: 500),
+  );
+
+  final StreamController<List<Movie>> _suggestionStreamController =
+      new StreamController.broadcast();
+  Stream<List<Movie>> get suggestionStream =>
+      this._suggestionStreamController.stream;
+
   MoviesProvider() {
     this.getInDisplayMoview();
     this.getpopularMovies();
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(
+    final url = Uri.https(
       this._baseUrl,
       endpoint,
       {
@@ -63,5 +76,35 @@ class MoviesProvider extends ChangeNotifier {
     moviesCast[movieId] = creditsresponse.cast;
 
     return creditsresponse.cast;
+  }
+
+  Future<List<Movie>> searchMovies(String query) async {
+    final url = Uri.https(
+      this._baseUrl,
+      '/3/search/movie',
+      {
+        'api_key': _apiKey,
+        'language': _language,
+        'query': '$query',
+      },
+    );
+
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+    return searchResponse.results;
+  }
+
+  void getSuggestionsByQuery(String searchTerm) {
+    debouncer.value = '';
+    debouncer.onValue = (value) async {
+      //print('Tenemos valor a buscar $value');
+      final results = await this.searchMovies(value);
+      this._suggestionStreamController.add(results);
+    };
+    final timer = Timer.periodic(Duration(milliseconds: 300), (_) {
+      debouncer.value = searchTerm;
+    });
+
+    Future.delayed(Duration(milliseconds: 301)).then((_) => timer.cancel());
   }
 }
--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -61,8 +61,10 @@ class _MovieSliderState extends State<MovieSlider> {
               controller: scrollController,
               scrollDirection: Axis.horizontal,
               itemCount: widget.movies.length,
-              itemBuilder: (_, int index) =>
-                  _MoviePoster(movie: widget.movies[index]),
+              itemBuilder: (_, int index) => _MoviePoster(
+                widget.movies[index],
+                '${widget.title}-${index}-${widget.movies[index].id}',
+              ),
             ),
           ),
         ],
@@ -73,13 +75,16 @@ class _MovieSliderState extends State<MovieSlider> {
 
 class _MoviePoster extends StatelessWidget {
   final Movie movie;
+  final String heroId;
 
-  const _MoviePoster({
-    Key? key,
-    required this.movie,
-  }) : super(key: key);
+  const _MoviePoster(
+    this.movie,
+    this.heroId,
+  );
   @override
   Widget build(BuildContext context) {
+    movie.heroId = heroId;
+
     return Container(
       width: 130,
       height: 190,
@@ -94,14 +99,17 @@ class _MoviePoster extends StatelessWidget {
               'details',
               arguments: movie,
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage(movie.fullPosterImg),
-                width: 130,
-                height: 190,
-                fit: BoxFit.cover,
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage(movie.fullPosterImg),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
