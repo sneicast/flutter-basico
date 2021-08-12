1f188b8ad0a85d6dc19496416dc25a9a56c8b736
Sneider Castillo <sneicast@hotmail.com>
2021-08-11 23:57:17 -0500

    Implementacion del primer disño

--- disenos/lib/screens/basic_design.dart
+++ disenos/lib/screens/basic_design.dart
@@ -4,11 +4,92 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/landscape.jpeg'),
-        ),
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage('assets/landscape.jpeg'),
+          ),
+
+          //title
+          Title(),
+          //Buttom Section
+          ButtonSection(),
+          //Descripcion
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+                'Ligula quam montes class pulvinar magnis sagittis rhoncus, dignissim cubilia dui dictum nulla nam fringilla, cum parturient augue ridiculus est suscipit. Etiam congue lobortis semper id mauris mattis, interdum euismod velit pretium tellus. Penatibus neque consequat nostra massa ridiculus lectus curae quis, commodo primis egestas fames arcu posuere habitasse, duis himenaeos vivamus eu taciti tristique id.'),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Kandersteg Switzerland',
+                  style: TextStyle(color: Colors.black45)),
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(Icons.star, color: Colors.red),
+          Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, text: "Call"),
+          CustomButton(icon: Icons.map_sharp, text: "Route"),
+          CustomButton(icon: Icons.share, text: "Share"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({Key? key, required this.icon, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue, size: 30),
+        Text(this.text, style: TextStyle(color: Colors.blue)),
+      ],
     );
   }
 }
